f3fad910db5d49974e3ccb1699989eacb54f3b20
diegosn79 <diego.s.naranjo@gmail.com>
2025-03-17 16:36:57 -0300

    Add files via upload

---
+++ change_internal_reference_for_sku/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Change internal reference for sku',
+    'version': '0.1',
+    'category': 'Sales',
+    'summary': 'This module customizes Odoo product views to replace the Internal Reference field label with SKU',
+    'description': """
+    This module customizes Odoo's product views to replace the "Internal Reference" field label with "SKU"
+
+    ===================================================
+    Changes "Internal Reference" to "SKU" in the admin product form and product list views.
+    Updates the product detail page on the website to display SKU above product attributes.
+    Uses Odoo's inheritance system (XPath) for clean and efficient modifications.
+    """,
+    'author': 'Diego Naranjo',
+    'category': 'Sales/Products',
+    'depends': ['sale',
+                'base',
+                'product',
+                'website_sale',
+                'website',],
+    'data': [
+            'views/product_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
